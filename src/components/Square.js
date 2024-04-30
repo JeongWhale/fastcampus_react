e24e1301fabac8f2d58d41f8d46764685c3c0dd1
JeongWhale <wook971215@gmail.com>
2024-04-30 21:21:04 +0900

    tictactoe

---
+++ src/components/Square.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Square.css";
+
+const Square = ({ onClick, value }) => {
+        return(
+            <div>
+                <button className="square"
+                 onClick={onClick}>
+                    {value}
+                </button>
+            </div>
+         )    
+}
+
+export default Square;
